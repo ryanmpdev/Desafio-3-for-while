a5fb730f04603bd546c126fc068b3ba91299d363
ryanmpdev <ryanmaciel697@gmail.com>
2022-06-16 18:37:39 -0300

    desaio 3 js

---
+++ script.js
@@ -0,0 +1,28 @@
+//numeros de 1 a 10
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log("números crescentes: ", numero);
+}
+
+//de 10 a 0
+
+let number = 10;
+while(number >=1){
+     console.log("número:", number);
+     number--;
+}
+
+//numeros par
+
+for(let par = 0; par <= 100; par++)
+     if(par %2 === 0 ){
+console.log(par);
+}
+
+//numeros impar
+
+for(let impar = 0; impar <= 100; impar++)
+     if(impar %2 === 1 ){
+console.log(impar);
+}
+
